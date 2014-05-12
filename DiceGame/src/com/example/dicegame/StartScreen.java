b91a6e3e9ffcc1924bc5123c69d2017967e92742
soerenl <soeren.leicht@googlemail.com>
2014-05-12 11:42:05 +0200

    Added start screen

---
+++ DiceGame/src/com/example/dicegame/StartScreen.java
@@ -0,0 +1,15 @@
+package com.example.dicegame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartScreen extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start_screen);
+	}
+
+}
